287788ffdc1a049fe85cc09d70d61130e9cbba4c
Fatumner <Fatumner@wp.pl>
2014-01-03 16:34:33 +0100

    wstepny interfejs z tymczasowym wczytywaniem i nazwami algorytmow

--- WI/WI/Form1.Designer.cs
+++ WI/WI/Form1.Designer.cs
@@ -28,34 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.openImage = new System.Windows.Forms.Button();
+            this.loadImages = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // splitContainer1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 96);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "WI";
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.openImage);
+            this.splitContainer1.Panel2.Controls.Add(this.loadImages);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 562);
+            this.splitContainer1.SplitterDistance = 392;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ItemSize = new System.Drawing.Size(58, 18);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(386, 556);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(378, 530);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 7);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(365, 517);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 517);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(378, 530);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(138, 238);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(7, 7);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(365, 517);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(1, 1);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // openImage
+            // 
+            this.openImage.Location = new System.Drawing.Point(112, 232);
+            this.openImage.Name = "openImage";
+            this.openImage.Size = new System.Drawing.Size(75, 23);
+            this.openImage.TabIndex = 1;
+            this.openImage.Text = "openImage";
+            this.openImage.UseVisualStyleBackColor = true;
+            this.openImage.Click += new System.EventHandler(this.openImage_Click);
+            // 
+            // loadImages
+            // 
+            this.loadImages.Location = new System.Drawing.Point(112, 202);
+            this.loadImages.Name = "loadImages";
+            this.loadImages.Size = new System.Drawing.Size(75, 23);
+            this.loadImages.TabIndex = 0;
+            this.loadImages.Text = "loadImages";
+            this.loadImages.UseVisualStyleBackColor = true;
+            this.loadImages.Click += new System.EventHandler(this.open_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 255);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button loadImages;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openImage;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
 
     }
 }
--- WI/WI/Form1.cs
+++ WI/WI/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+using System.IO;
 
 namespace WI
 {
     public partial class Form1 : Form
     {
+        List<FileInfo> files = new List<FileInfo>();
         public Form1()
         {
             InitializeComponent();
+            this.listView1.View = View.LargeIcon;
+            //this.imageList1.ImageSize = new Size(256, 256);
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.LargeImageList.ImageSize = new Size(32, 32);
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            //backgroundWorker.RunWorkerAsync();
+            this.folderBrowserDialog1.ShowDialog();
+            try
+            {
+                this.files.Clear();
+                this.listView1.Clear();
+                this.imageList1.Images.Clear();
+                DirectoryInfo dir = new DirectoryInfo(this.folderBrowserDialog1.SelectedPath);
+                this.progressBar1.Maximum = dir.GetFiles().GetLength(0);
+                this.progressBar1.Step = 1;
+                this.progressBar1.Value = 0;
+                this.progressBar1.Show();
+                int j = 0;
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    try
+                    {
+                        this.imageList1.Images.Add(Image.FromFile(file.FullName));
+                        this.files.Add(file);
+                        ListViewItem item = new ListViewItem();
+                        item.ImageIndex = j;
+                        this.listView1.Items.Add(item);
+                        j++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("This is not an image file");
+                    }
+                    ++this.progressBar1.Value;
+                }
+                this.progressBar1.Hide();
+            }
+            catch
+            {
+            }
+        }
+
+        private void openImage_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+            try
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+            catch (IOException)
+            { 
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+
+            //PictureBox picture = new PictureBox();
+            pictureBox1.Image = this.imageList1.Images[this.listView1.FocusedItem.Index]; //this.listView1.GetItemAt(this.listView1.FocusedItem.Position.X, this.listView1.FocusedItem.Position.Y);
+            //picture.Size = new Size(150, 250);
+            //picture.Show();
+            System.Console.Out.WriteLine(files[this.listView1.FocusedItem.Index].FullName);
+            Form2 form = new Form2(files[this.listView1.FocusedItem.Index].FullName);
+            form.Show();
+            System.Console.Out.WriteLine("dziala");
         }
     }
 }
